46c1aa2b1e6c953ff921d2161cc688859fb87cc1
Daniel Brakke <brakscript@gmail.com>
2020-11-26 10:16:01 -0600

    create pert estimator

---
+++ src/PertEstimator.js
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+
+function PertEstimator() {
+
+  const [pessimistic, setPessimistic] = useState(0);
+  const [realistic, setRealistic] = useState(0);
+  const [optimistic, setOptimistic] = useState(0);
+
+  const getPert = () => {
+    if ([realistic, pessimistic, optimistic].every(n => !n || n < 0)) return 0;
+    if (pessimistic < 0) setPessimistic(0);
+    if (realistic < 0) setRealistic(0);
+    if (optimistic < 0) setOptimistic(0);
+
+    const estimate = (pessimistic + (4 * realistic) + optimistic) / 6;
+
+    return getRoundedUpToQuarter(estimate);
+  };
+
+  const getRoundedUpToQuarter = number => {
+    return Math.ceil(number * 4) / 4;
+  };
+
+  const pert = getPert();
+  const documentation = getRoundedUpToQuarter(pert / 5);
+  const steps = ".25"
+  const scenarios = {
+    "Pessimistic": {
+      "value": pessimistic,
+      "onChange": setPessimistic
+    },
+    "Realistic": {
+      "value": realistic,
+      "onChange": setRealistic
+    },
+    "Optimistic": {
+      "value": optimistic,
+      "onChange": setOptimistic
+    },
+  }
+
+  return (
+    <div>
+      <h1>PERT Estimator</h1>
+      <p>Fill in the fields with your hour estimate for the task at hand</p>
+      <div className="inputs">
+        {
+          Object.entries(scenarios).map(([scenario, properties]) => {
+            return <div className="pert_scenario">
+              <label htmlFor={"pert_"+scenario.toLowerCase()}>{scenario}</label>
+              <input
+                type="number"
+                name={"pert_"+scenario.toLowerCase()}
+                step={steps}
+                min={0}
+                value={properties.value}
+                onChange={e => properties.onChange(parseFloat(e.target.value))}
+              />
+            </div>
+          })
+        }
+      </div>
+      <div className="results">
+        {pert ? (
+            <h2>
+              Estimate: <span style={{color: "#EE7623"}}>{pert} hours</span>
+            </h2>
+          ) : 'Fill in the numbers, silly.'
+        }
+      </div>
+      <h3>
+        {!!pert && `Also, you should probably consider ~${documentation} hours for documentation 😁`}
+      </h3>
+    </div>
+  );
+}
+
+export default PertEstimator;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import PertEstimator from './PertEstimator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PertEstimator />
   </React.StrictMode>,
   document.getElementById('root')
 );
